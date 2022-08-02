e064fa2a4ace44a2b7a9737ba756501202fe20a0
Xojiakbar994 <94231309+Xojiakbar994@users.noreply.github.com>
2022-08-02 23:56:54 +0500

    add material modules

--- src/app/wash-entry-form/wash-entry-form.module.ts
+++ src/app/wash-entry-form/wash-entry-form.module.ts
@@ -3,15 +3,20 @@ import { CommonModule } from '@angular/common';
 
 import { WashEntryFormRoutingModule } from './wash-entry-form-routing.module';
 import { WashEntryFormComponent } from './wash-entry-form.component';
-
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
-  declarations: [
-    WashEntryFormComponent
-  ],
+  declarations: [WashEntryFormComponent],
   imports: [
     CommonModule,
-    WashEntryFormRoutingModule
-  ]
+    WashEntryFormRoutingModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatIconModule,
+  ],
 })
-export class WashEntryFormModule { }
+export class WashEntryFormModule {}
