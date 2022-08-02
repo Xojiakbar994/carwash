a9224ec84e00be2e513cd995e41b81be730f1367
Xojiakbar994 <94231309+Xojiakbar994@users.noreply.github.com>
2022-08-03 00:13:35 +0500

    added other fields

--- src/app/wash-entry-form/wash-entry-form.module.ts
+++ src/app/wash-entry-form/wash-entry-form.module.ts
@@ -7,6 +7,7 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { MatSelectModule } from '@angular/material/select';
 
 @NgModule({
   declarations: [WashEntryFormComponent],
@@ -17,6 +18,7 @@ import { MatIconModule } from '@angular/material/icon';
     MatInputModule,
     MatButtonModule,
     MatIconModule,
+    MatSelectModule,
   ],
 })
 export class WashEntryFormModule {}
