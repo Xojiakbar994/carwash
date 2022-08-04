9d7c5ee086b64cfd819223790bc2750633a88f1e
Xojiakbar994 <94231309+Xojiakbar994@users.noreply.github.com>
2022-08-04 22:33:52 +0500

    add table data

--- src/app/wash-entry-list/wash-entry-list.component.ts
+++ src/app/wash-entry-list/wash-entry-list.component.ts
@@ -1,15 +1,107 @@
-import { Component, OnInit } from '@angular/core';
+import { LiveAnnouncer } from '@angular/cdk/a11y';
+import { AfterViewInit, Component, ViewChild } from '@angular/core';
+import { MatSort, Sort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
 
+export interface WashEntry {
+  washer: string;
+  date: string;
+  plateNumber: string;
+  model: string;
+  color: string;
+  carBodyType: string;
+  serviceType: string;
+  price: string;
+}
+
+const ELEMENT_DATA: WashEntry[] = [
+  {
+    date: '04.08.2022',
+    washer: 'Alish',
+    plateNumber: '01L230GB',
+    model: 'Cobalt',
+    color: 'white',
+    carBodyType: 'sedan',
+    serviceType: 'polniy',
+    price: '50000',
+  },
+  {
+    date: '04.08.2022',
+    washer: 'Botir',
+    plateNumber: '01L889GB',
+    model: 'Lacetti',
+    color: 'white',
+    carBodyType: 'sedan',
+    serviceType: 'polniy',
+    price: '50000',
+  },
+  {
+    date: '02.08.2022',
+    washer: 'Qodir',
+    plateNumber: '01L851GB',
+    model: 'Matiz',
+    color: 'white',
+    carBodyType: 'sedan',
+    serviceType: 'polniy',
+    price: '30000',
+  },
+  {
+    date: '01.08.2022',
+    washer: 'Ali',
+    plateNumber: '01L456GB',
+    model: 'Cobalt',
+    color: 'white',
+    carBodyType: 'sedan',
+    serviceType: 'polniy',
+    price: '50000',
+  },
+  {
+    date: '03.08.2022',
+    washer: 'Hayrulla',
+    plateNumber: '01L987GB',
+    model: 'Nexia',
+    color: 'white',
+    carBodyType: 'sedan',
+    serviceType: 'polniy',
+    price: '45000',
+  },
+];
 @Component({
   selector: 'app-wash-entry-list',
   templateUrl: './wash-entry-list.component.html',
-  styleUrls: ['./wash-entry-list.component.scss']
+  styleUrls: ['./wash-entry-list.component.scss'],
 })
-export class WashEntryListComponent implements OnInit {
+export class WashEntryListComponent implements AfterViewInit {
+  displayedColumns: string[] = [
+    'washer',
+    'date',
+    'plateNumber',
+    'model',
+    'color',
+    'carBodyType',
+    'serviceType',
+    'price',
+  ];
+  dataSource = new MatTableDataSource(ELEMENT_DATA);
 
-  constructor() { }
+  constructor(private _liveAnnouncer: LiveAnnouncer) {}
 
-  ngOnInit(): void {
+  @ViewChild(MatSort) sort!: MatSort;
+
+  ngAfterViewInit() {
+    this.dataSource.sort = this.sort;
   }
 
+  /** Announce the change in sort state for assistive technology. */
+  announceSortChange(sortState: Sort) {
+    // This example uses English messages. If your application supports
+    // multiple language, you would internationalize these strings.
+    // Furthermore, you can customize the message to add additional
+    // details about the values being sorted.
+    if (sortState.direction) {
+      this._liveAnnouncer.announce(`Sorted ${sortState.direction}ending`);
+    } else {
+      this._liveAnnouncer.announce('Sorting cleared');
+    }
+  }
 }
--- src/app/wash-entry-list/wash-entry-list.module.ts
+++ src/app/wash-entry-list/wash-entry-list.module.ts
@@ -1,17 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatTableModule } from '@angular/material/table';
+import { MatSortModule } from '@angular/material/sort';
 
 import { WashEntryListRoutingModule } from './wash-entry-list-routing.module';
 import { WashEntryListComponent } from './wash-entry-list.component';
 
-
 @NgModule({
-  declarations: [
-    WashEntryListComponent
-  ],
-  imports: [
-    CommonModule,
-    WashEntryListRoutingModule
-  ]
+  declarations: [WashEntryListComponent],
+  imports: [CommonModule, MatTableModule, MatSortModule, WashEntryListRoutingModule],
 })
-export class WashEntryListModule { }
+export class WashEntryListModule {}
