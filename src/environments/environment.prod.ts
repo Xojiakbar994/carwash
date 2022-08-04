986e47222060b1965f90d1a3a5fea27c1c22a437
Xojiakbar994 <94231309+Xojiakbar994@users.noreply.github.com>
2022-08-04 22:46:19 +0500

    add firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
+  firebase: {
+    projectId: 'genesis-5e6f8',
+    appId: '1:606223995139:web:1cef9123e92a2102fe1936',
+    storageBucket: 'genesis-5e6f8.appspot.com',
+    locationId: 'europe-west',
+    apiKey: 'AIzaSyBAwLLCxFihMfTQet4n-TAAED9ZbwLJLJg',
+    authDomain: 'genesis-5e6f8.firebaseapp.com',
+    messagingSenderId: '606223995139',
+    measurementId: 'G-NHKVYR0LFG',
+  },
   production: true
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'genesis-5e6f8',
+    appId: '1:606223995139:web:1cef9123e92a2102fe1936',
+    storageBucket: 'genesis-5e6f8.appspot.com',
+    locationId: 'europe-west',
+    apiKey: 'AIzaSyBAwLLCxFihMfTQet4n-TAAED9ZbwLJLJg',
+    authDomain: 'genesis-5e6f8.firebaseapp.com',
+    messagingSenderId: '606223995139',
+    measurementId: 'G-NHKVYR0LFG',
+  },
   production: false
 };
 
