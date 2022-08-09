3c83ed2cd1e2d515543008d7430381f31196301a
Xojiakbar994 <94231309+Xojiakbar994@users.noreply.github.com>
2022-08-09 21:51:27 +0500

    add formGroup

--- src/app/wash-entry-form/wash-entry-form.component.ts
+++ src/app/wash-entry-form/wash-entry-form.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-wash-entry-form',
   templateUrl: './wash-entry-form.component.html',
-  styleUrls: ['./wash-entry-form.component.scss']
+  styleUrls: ['./wash-entry-form.component.scss'],
 })
 export class WashEntryFormComponent implements OnInit {
+  entryForm = new FormGroup({
+    washer: new FormControl('Alish'),
+    date: new FormControl('10/08/2022'),
+    plateNumber: new FormControl(''),
+    model: new FormControl(''),
+    color: new FormControl(''),
+    carBodyType: new FormControl(''),
+    serviceType: new FormControl(''),
+    price: new FormControl(''),
+  });
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  save() {
+    //save the new document into firebase storage
+    console.log('ozim tushunadigan tekst');
   }
-
 }
--- src/app/wash-entry-form/wash-entry-form.module.ts
+++ src/app/wash-entry-form/wash-entry-form.module.ts
@@ -8,6 +8,7 @@ import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatSelectModule } from '@angular/material/select';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [WashEntryFormComponent],
@@ -19,6 +20,7 @@ import { MatSelectModule } from '@angular/material/select';
     MatButtonModule,
     MatIconModule,
     MatSelectModule,
+    ReactiveFormsModule,
   ],
 })
 export class WashEntryFormModule {}
