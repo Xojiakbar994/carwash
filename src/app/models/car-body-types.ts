11628342bd988814a21b32170c543b0c0a6dd5c2
Xojiakbar994 <94231309+Xojiakbar994@users.noreply.github.com>
2022-08-23 22:01:45 +0500

    add constantes

---
+++ src/app/models/car-body-types.ts
@@ -0,0 +1,5 @@
+export const CAR_BODY_TYPES = [
+  'A-B-C tur kuzov (Matiz, Nexia, Cobolt)',
+  'D-E-F tur kuzov (Malibu, Camry)',
+  'Krosover miniven (Captiva, Jeep)',
+];
---
+++ src/app/models/service-types.ts
@@ -0,0 +1,8 @@
+export const SERVICE_TYPES = [
+  'Polniy moyka',
+  'Kuzov moyka',
+  'Artish, vosk, quritish',
+  'Salon tozalash',
+  'Karcher',
+  'Ximchistka',
+];
